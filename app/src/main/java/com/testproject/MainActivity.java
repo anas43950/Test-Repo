bfd42f5bf3569c7447ba2e1aa1eb1a989b9eef9b
Anas Ansari <76153513+anas43950@users.noreply.github.com>
2023-04-02 11:18:43 +0530

    Hmmm

---
+++ app/src/main/java/com/testproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Paste it somewhere, you will need it when pushing file
+    }
+}
